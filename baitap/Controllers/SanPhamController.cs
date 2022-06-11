dc667e9e4db60d7813f2f51735ad4af67207bf07
Anh Quang <anhquang10xvn@outlook.com>
2022-06-11 08:35:21 +0700

    xoa using thua

--- baitap/Controllers/SanPhamController.cs
+++ baitap/Controllers/SanPhamController.cs
@@ -1,11 +1,8 @@
 ﻿using baitap.Data;
 using baitap.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace baitap.Controllers
 {
